25f9575a9e0a798613313723643cf1f8e9faac89
heriher76 <herhermawan007@gmail.com>
2019-08-10 08:14:56 +0700

    fixregisterlogin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,15 +15,15 @@
           <!-- Wrapper for slides -->
           <div class="carousel-inner" style="border-radius: 10%; min-height: 40vh; max-height: 40vh;">
             <div class="item active">
-              <img src="{{ url('slider/a.jpeg') }}" alt="Slider Pertama" style="height: 40vh; width: auto;">
+              <img src="{{ url('slider/a.jpeg') }}" alt="Slider Pertama" style="height: 40vh; width: 100%;">
             </div>
 
             <div class="item">
-              <img src="{{ url('slider/b.jpg') }}" alt="Slider Kedua" style="height: 40vh; width: auto;">
+              <img src="{{ url('slider/b.jpg') }}" alt="Slider Kedua" style="height: 40vh; width: 100%;">
             </div>
 
             <div class="item">
-              <img src="{{ url('slider/c.jpeg') }}" alt="Slider Ketiga" style="height: 40vh; width: auto;">
+              <img src="{{ url('slider/c.jpeg') }}" alt="Slider Ketiga" style="height: 40vh; width: 100%;">
             </div>
           </div>
 
